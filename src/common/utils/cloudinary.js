9a014299c28d1580c075cde2665ba35a5a2e804b
omaryasser01 <89039233+omaryasser01@users.noreply.github.com>
2026-04-03 23:51:18 +0200

    Delete src/common/utils/cloudinary.js

--- src/common/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dhukgeysn",
-  api_key: "452285255719831",
-  api_secret: "2FtXdYYkjKdraZXL_VeHLcMO5nI",
-});
-
-export default cloudinary;
